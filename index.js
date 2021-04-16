6662aa3c58a7829386d90abf39bf6ed9b93bc2f1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-16 01:22:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,4 +50,4 @@ const [, , , , ,indg,] = colors;
 const { muppetName, color, song, job, partner } = muppet;
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const { song2,song4} = nestedMuppet.album.theMuppetMovie;
-const { nestedJob, nestedPartner } = 'nestedMuppet, nestedPartner'.split(" ")
+const { nestedJob } = nestedMuppet.nestedJob;
