37af993b94db52e3dfa9d69dfa1e458694d4a4e8
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-16 00:57:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,4 +46,3 @@ const [ moo] = cow;
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
-
